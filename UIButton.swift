71779940d7a17b431fb3b950cbde1b2b719d11dc
Bohdan Savych <sssbohdan@gmail.com>
2019-06-13 16:15:55 +0300

    Update UIButton.swift

--- UIButton.swift
+++ UIButton.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2016 BBB. All rights reserved.
 //
 
+import Foundation
 import UIKit
 
 typealias ContentPosition = (horizontal: HorizontalPosition, vertical: VerticalPosition)
@@ -24,18 +25,13 @@ enum VerticalPosition {
 }
 
 extension UIButton {
-    func setupImageInButton(imagePosition: ContentPosition? = nil,
-                            imageOffset: ContentOffset? = nil,
-                            textPosition: ContentPosition? = nil,
-                            textOffset: ContentOffset? = nil) {
+    func setupImageInButton(imagePosition: ContentPosition = (.center, .center),
+                            imageOffset: ContentOffset = (0, 0),
+                            textPosition: ContentPosition = (.center, .center),
+                            textOffset: ContentOffset = (0, 0)) {
         guard let imageViewFrame = imageView?.frame, let labelFrame = titleLabel?.frame else {
                 return
         }
-        
-        let imagePosition = imagePosition ?? (.center, .center)
-        let imageOffset = imageOffset ?? (0, 0)
-        let textPosition = textPosition ?? (.center, .center)
-        let textOffset = textOffset ?? (0, 0)
         
         contentHorizontalAlignment = .left
         contentVerticalAlignment = .top
@@ -45,47 +41,47 @@ extension UIButton {
         
         switch imagePosition.horizontal {
         case .left:
-            imageEdgeInsets = UIEdgeInsetsMake(0, CGFloat(imageOffset.horizontal), 0, 0)
+            imageEdgeInsets = UIEdgeInsets.init(top: 0, left: CGFloat(imageOffset.horizontal), bottom: 0, right: 0)
         case .right:
             let imageViewXOrigin = buttonWidth - imageViewFrame.width
-            imageEdgeInsets = UIEdgeInsetsMake(0, imageViewXOrigin - CGFloat(imageOffset.horizontal), 0, 0)
+            imageEdgeInsets = UIEdgeInsets.init(top: 0, left: imageViewXOrigin - CGFloat(imageOffset.horizontal), bottom: 0, right: 0)
         case .center:
             let imageViewXOrigin = (buttonWidth - imageViewFrame.width) / 2.0
-            imageEdgeInsets = UIEdgeInsetsMake(0, imageViewXOrigin, 0, 0)
+            imageEdgeInsets = UIEdgeInsets.init(top: 0, left: imageViewXOrigin, bottom: 0, right: 0)
         }
         
         switch imagePosition.vertical {
         case .top:
-            imageEdgeInsets = UIEdgeInsetsMake(CGFloat(imageOffset.vertical),imageEdgeInsets.left, 0, imageEdgeInsets.right)
+            imageEdgeInsets = UIEdgeInsets.init(top: CGFloat(imageOffset.vertical),left: imageEdgeInsets.left, bottom: 0, right: imageEdgeInsets.right)
         case .bottom:
             let imageViewYOrigin = buttonHeight - imageViewFrame.height
-            imageEdgeInsets = UIEdgeInsetsMake(imageViewYOrigin - CGFloat(imageOffset.vertical), imageEdgeInsets.left, 0, imageEdgeInsets.right)
+            imageEdgeInsets = UIEdgeInsets.init(top: imageViewYOrigin - CGFloat(imageOffset.vertical), left: imageEdgeInsets.left, bottom: 0, right: imageEdgeInsets.right)
         case .center:
             let imageViewYOrigin = (buttonHeight - imageViewFrame.height) / 2.0
-            imageEdgeInsets = UIEdgeInsetsMake(imageViewYOrigin, imageEdgeInsets.left, 0, imageEdgeInsets.right)
+            imageEdgeInsets = UIEdgeInsets.init(top: imageViewYOrigin, left: imageEdgeInsets.left, bottom: 0, right: imageEdgeInsets.right)
         }
         
         switch textPosition.horizontal {
         case .left:
             let textXOrigin = -imageViewFrame.width + CGFloat(textOffset.horizontal)
-            titleEdgeInsets = UIEdgeInsetsMake(0, textXOrigin, 0, 0)
+            titleEdgeInsets = UIEdgeInsets.init(top: 0, left: textXOrigin, bottom: 0, right: 0)
         case .center:
             let textXOrigin = ((buttonWidth - labelFrame.width) / 2.0) - imageViewFrame.width
-            titleEdgeInsets = UIEdgeInsetsMake(0, textXOrigin, 0, 0)
+            titleEdgeInsets = UIEdgeInsets.init(top: 0, left: textXOrigin, bottom: 0, right: 0)
         case .right:
             let textXOrigin = (buttonWidth - labelFrame.width - CGFloat(textOffset.horizontal) - imageViewFrame.width)
-            titleEdgeInsets = UIEdgeInsetsMake(0, textXOrigin, 0, 0)
+            titleEdgeInsets = UIEdgeInsets.init(top: 0, left: textXOrigin, bottom: 0, right: 0)
         }
         
         switch textPosition.vertical {
         case .top:
-            titleEdgeInsets = UIEdgeInsetsMake(CGFloat(textOffset.vertical), titleEdgeInsets.left, 0, titleEdgeInsets.right)
+            titleEdgeInsets = UIEdgeInsets.init(top: CGFloat(textOffset.vertical), left: titleEdgeInsets.left, bottom: 0, right: titleEdgeInsets.right)
         case .center:
             let textYOrigin = (buttonHeight - labelFrame.height) / 2.0
-            titleEdgeInsets = UIEdgeInsetsMake(textYOrigin, titleEdgeInsets.left, 0, titleEdgeInsets.right)
+            titleEdgeInsets = UIEdgeInsets.init(top: textYOrigin, left: titleEdgeInsets.left, bottom: 0, right: titleEdgeInsets.right)
         case .bottom:
             let textYOrigin = buttonHeight - labelFrame.height - CGFloat(textOffset.vertical)
-            titleEdgeInsets = UIEdgeInsetsMake(textYOrigin, titleEdgeInsets.left, 0, titleEdgeInsets.right)
+            titleEdgeInsets = UIEdgeInsets.init(top: textYOrigin, left: titleEdgeInsets.left, bottom: 0, right: titleEdgeInsets.right)
         }
     }
 }
